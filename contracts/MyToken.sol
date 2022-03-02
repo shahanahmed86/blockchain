52dc0d1c108793676c64f4653917440b51c6d1ba
Shahan <shahanahmed86@github.com>
2022-03-02 16:35:40 +0500

    3. Delegated Transfer

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,14 +1,24 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.4.2 <0.9.0;
 
 contract MyToken {
+    // variables
     string public name = "My Token";
     string public symbol = "MY";
     string public standard = "My Token v1.0";
     uint256 public totalSupply;
 
+    // events
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
+    // variable mappings
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     // constructor
     constructor(uint256 _initialSupply) {
@@ -27,6 +37,37 @@ contract MyToken {
         balanceOf[_to] += _value;
 
         emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    // approve
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    // transferFrom
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(balanceOf[_from] >= _value);
+        require(allowance[_from][msg.sender] >= _value);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
 
         return true;
     }
--- test/MyToken.js
+++ test/MyToken.js
@@ -28,11 +28,11 @@ contract('MyToken', (accounts) => {
 	it('transfers token ownership', async () => {
 		const instance = await MyToken.deployed();
 		try {
-			const result = await instance.transfer.call(to, 99999999999999999999999);
+			await instance.transfer.call(to, 99999999999999999999999);
 		} catch (error) {
 			assert(error.message.indexOf('overflow') !== -1, 'error message must contain overflow');
 			const success = await instance.transfer.call(to, 250000, { from });
-      assert.equal(success, true, 'it returns true');
+			assert.equal(success, true, 'it returns true');
 
 			const receipt = await instance.transfer(to, 250000, { from });
 			assert.equal(receipt.logs.length, 1, 'triggers one event');
@@ -46,8 +46,69 @@ contract('MyToken', (accounts) => {
 
 			const fromBalance = await instance.balanceOf(from);
 			assert.equal(fromBalance.toNumber(), 750000, 'deduct the amount from the sending account');
+		}
+	});
 
+	it('approves tokens for  delegated transfer', async () => {
+		const instance = await MyToken.deployed();
 
+		const success = await instance.approve.call(to, 100);
+		assert.equal(success, true, 'it returns true');
+
+		const receipt = await instance.approve(to, 100, { from });
+		assert.equal(receipt.logs.length, 1, 'triggers one event');
+		assert.equal(receipt.logs[0].event, 'Approval', 'should be the Approval event');
+		assert.equal(receipt.logs[0].args._owner, from, 'logs the account the tokens are authorized by');
+		assert.equal(receipt.logs[0].args._spender, to, 'logs the account the tokens are authorized to');
+		assert.equal(receipt.logs[0].args._value, 100, 'logs the transfer amount');
+
+		const allowance = await instance.allowance(from, to);
+		assert.equal(allowance.toNumber(), 100, 'stores the allowance for delegated transfer');
+	});
+
+	it('handles delegated token transfers', async () => {
+		const fromAccount = accounts[2];
+		const toAccount = accounts[3];
+		const spendingAccount = accounts[4];
+
+		const instance = await MyToken.deployed();
+		try {
+			await instance.transfer(fromAccount, 100, { from });
+
+			await instance.approve(spendingAccount, 10, { from: fromAccount });
+
+			await instance
+				.transferFrom(fromAccount, toAccount, 9999, { from: spendingAccount })
+				.then((receipt) => receipt.toNumber());
+		} catch (error) {
+			assert(error.message.indexOf('revert') !== -1, 'cannot transfer value larger than balance');
+
+			try {
+				await instance
+					.transferFrom(fromAccount, toAccount, 20, { from: spendingAccount })
+					.then((receipt) => receipt.toNumber());
+			} catch (error) {
+				assert(error.message.indexOf('revert') !== -1, 'cannot transfer value larger than approved amount');
+
+				const success = await instance.transferFrom.call(fromAccount, toAccount, 10, { from: spendingAccount });
+				assert.equal(success, true, 'it returns true');
+
+        const receipt = await instance.transferFrom(fromAccount, toAccount, 10, { from: spendingAccount });
+        assert.equal(receipt.logs.length, 1, 'triggers one event');
+        assert.equal(receipt.logs[0].event, 'Transfer', 'should be the Transfer event');
+        assert.equal(receipt.logs[0].args._from, fromAccount, 'logs the account the tokens are transferred by');
+        assert.equal(receipt.logs[0].args._to, toAccount, '100 the account the tokens are transferred to');
+        assert.equal(receipt.logs[0].args._value, 10, 'logs the transfer amount');
+
+        const fromBalance = await instance.balanceOf(fromAccount);
+        assert.equal(fromBalance.toNumber(), 90, 'deducts the amount from the sending account');
+
+        const toBalance = await instance.balanceOf(toAccount);
+        assert.equal(toBalance.toNumber(), 10, 'adds the amount to the receiving account');
+
+        const allowance = await instance.allowance(fromAccount, spendingAccount);
+        assert.equal(allowance.toNumber(), 0, 'deducts the amount from the allowance');
+			}
 		}
 	});
 });

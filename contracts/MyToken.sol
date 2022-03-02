525847796f13f32a387013bf9c4990e2c48625f2
Shahan <shahanahmed86@github.com>
2022-03-02 13:52:48 +0500

    2. Transfer token

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,12 +1,33 @@
 pragma solidity >=0.4.2 <0.9.0;
 
 contract MyToken {
-  // constructor
-  // set the total number of tokens
-  // read the total number of tokens
-  uint256 public totalSupply;
+    string public name = "My Token";
+    string public symbol = "MY";
+    string public standard = "My Token v1.0";
+    uint256 public totalSupply;
 
-  constructor() {
-    totalSupply = 1000000;
-  }
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    mapping(address => uint256) public balanceOf;
+
+    // constructor
+    constructor(uint256 _initialSupply) {
+        totalSupply = _initialSupply;
+        balanceOf[msg.sender] = _initialSupply;
+    }
+
+    // transfer
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(balanceOf[msg.sender] >= _value);
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
 }
--- test/MyToken.js
+++ test/MyToken.js
@@ -1,12 +1,53 @@
 const MyToken = artifacts.require('MyToken');
 
 contract('MyToken', (accounts) => {
-  it ('sets the total supply upon deployment', () => {
-    return MyToken.deployed().then(instance => {
-      tokenInstance = instance;
-      return tokenInstance.totalSupply();
-    }).then(totalSupply => {
-      assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to one million');
-    })
-  })
-})
+	const to = accounts[1];
+	const from = accounts[0];
+
+	it('initializes the contract with the correct value', async () => {
+		const instance = await MyToken.deployed();
+		const name = await instance.name();
+		assert.equal(name, 'My Token', 'has the correct name');
+
+		const symbol = await instance.symbol();
+		assert.equal(symbol, 'MY', 'has the correct value');
+
+		const standard = await instance.standard();
+		assert.equal(standard, 'My Token v1.0', 'has the correct standard');
+	});
+
+	it('allocates the initial supply upon deployment', async () => {
+		const instance = await MyToken.deployed();
+		const totalSupply = await instance.totalSupply();
+		assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to one million');
+
+		const adminBalance = await instance.balanceOf(from);
+		assert.equal(adminBalance.toNumber(), 1000000, 'it allocates the initial supply to the admin account');
+	});
+
+	it('transfers token ownership', async () => {
+		const instance = await MyToken.deployed();
+		try {
+			const result = await instance.transfer.call(to, 99999999999999999999999);
+		} catch (error) {
+			assert(error.message.indexOf('overflow') !== -1, 'error message must contain overflow');
+			const success = await instance.transfer.call(to, 250000, { from });
+      assert.equal(success, true, 'it returns true');
+
+			const receipt = await instance.transfer(to, 250000, { from });
+			assert.equal(receipt.logs.length, 1, 'triggers one event');
+			assert.equal(receipt.logs[0].event, 'Transfer', 'should be the Transfer event');
+			assert.equal(receipt.logs[0].args._from, from, 'logs the account the tokens are transferred from');
+			assert.equal(receipt.logs[0].args._to, to, 'logs the account the tokens are transferred to');
+			assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount');
+
+			const toBalance = await instance.balanceOf(to);
+			assert.equal(toBalance.toNumber(), 250000, 'add the amount to the receiving account');
+
+			const fromBalance = await instance.balanceOf(from);
+			assert.equal(fromBalance.toNumber(), 750000, 'deduct the amount from the sending account');
+
+
+		}
+	});
+});
